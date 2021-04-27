b119da4971dbe6f184dbbf296d378faeaff95c00
Pritam <pritamdebnath297@gmail.com>
2021-04-27 23:44:58 +0530

    chore: recommends component added

---
+++ src/components/Recommends.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Recommends = (props) => {
+    return(
+        <div>
+            <h3>
+                Recommends
+            </h3>
+        </div>
+    )
+};
+
+export default Recommends;
