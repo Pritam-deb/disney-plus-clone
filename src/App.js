82c93757a15bbb745ef6387f41f2cadabd3c396a
Pritam <pritamdebnath297@gmail.com>
2021-04-27 20:10:47 +0530

    style: added gradient to home

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Login from './components/Login';
 import Header from './components/Header';
+import Home from './components/Home';
+
 function App() {
   return (
     <div className="App">
@@ -10,6 +12,9 @@ function App() {
         <Switch>
           <Route exact path='/'>
             <Login/>
+          </Route>
+          <Route path="/home">
+            <Home>   </Home>
           </Route>
         </Switch>
       </Router>
