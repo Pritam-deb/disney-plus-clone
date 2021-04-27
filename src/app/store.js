efc57d71a67cf25f0778bf285defce4bff1c0fd5
Pritam <pritamdebnath297@gmail.com>
2021-04-28 02:08:20 +0530

    Revert ":-1:"
    This reverts commit 0554ebcd82da4616c868966984f3bc6ff3c68af0.

--- src/app/store.js
+++ src/app/store.js
@@ -1,13 +1,12 @@
-import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
-import userReducer from "../features/user/userSlice";
-import movieReducer from "../features/movie/movieSlice";
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import userReducer from '../features/user/userSlice'
+
 
 export default configureStore({
-  reducer: {
-    user: userReducer,
-    movie: movieReducer,
-  },
-  middleware: getDefaultMiddleware({
-    serializableCheck: false,
-  }),
-});
+    reducer: {
+        user: userReducer
+    },
+    middleware: getDefaultMiddleware({
+        serializableCheck: false,
+    })
+})
