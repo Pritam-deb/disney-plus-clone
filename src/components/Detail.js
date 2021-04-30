cd376fceba61e76a868380d21c839ec1f45bec35
Pritam <pritamdebnath297@gmail.com>
2021-04-30 16:07:11 +0530

    style: detail page

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -2,8 +2,20 @@ import styled from 'styled-components'
 
 const Detail = (props) => {
     return(
-        <h4>Details</h4>
+        <Container>Details</Container>
     )
-}
+};
+
+const Container = styled.div `
+    position: relative;
+    min-height: calc(100vh - 250px);
+    overflow-x: hidden;
+    display: block;
+    top: 72px;
+    padding: 0 calc(3.5vw - 5px);
+
+`;
+
+
 
 export default Detail;
